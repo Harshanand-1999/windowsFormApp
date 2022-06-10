54d010d22c002f3e94950367b2724d695cc8d9e1
Harsh Anand <harshanand1198@gmail.com>
2022-06-10 14:16:00 +0530

    first commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.IDtextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NAMEtextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AGEtextbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.STANDARDtextbox = new System.Windows.Forms.TextBox();
+            this.PREVIOUSbutton = new System.Windows.Forms.Button();
+            this.NEXTbutton = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.GENDERtextbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(215, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(353, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "student information";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // IDtextBox
+            // 
+            this.IDtextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDtextBox.Location = new System.Drawing.Point(486, 101);
+            this.IDtextBox.Name = "IDtextBox";
+            this.IDtextBox.Size = new System.Drawing.Size(169, 30);
+            this.IDtextBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(249, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(249, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name";
+            // 
+            // NAMEtextbox
+            // 
+            this.NAMEtextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NAMEtextbox.Location = new System.Drawing.Point(486, 158);
+            this.NAMEtextbox.Name = "NAMEtextbox";
+            this.NAMEtextbox.Size = new System.Drawing.Size(169, 30);
+            this.NAMEtextbox.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(249, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 32);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Age";
+            // 
+            // AGEtextbox
+            // 
+            this.AGEtextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AGEtextbox.Location = new System.Drawing.Point(486, 209);
+            this.AGEtextbox.Name = "AGEtextbox";
+            this.AGEtextbox.Size = new System.Drawing.Size(169, 30);
+            this.AGEtextbox.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(249, 325);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 32);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Standard";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // STANDARDtextbox
+            // 
+            this.STANDARDtextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.STANDARDtextbox.Location = new System.Drawing.Point(486, 325);
+            this.STANDARDtextbox.Name = "STANDARDtextbox";
+            this.STANDARDtextbox.Size = new System.Drawing.Size(169, 30);
+            this.STANDARDtextbox.TabIndex = 7;
+            // 
+            // PREVIOUSbutton
+            // 
+            this.PREVIOUSbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PREVIOUSbutton.Location = new System.Drawing.Point(277, 389);
+            this.PREVIOUSbutton.Name = "PREVIOUSbutton";
+            this.PREVIOUSbutton.Size = new System.Drawing.Size(117, 36);
+            this.PREVIOUSbutton.TabIndex = 9;
+            this.PREVIOUSbutton.Text = "previous";
+            this.PREVIOUSbutton.UseVisualStyleBackColor = true;
+            this.PREVIOUSbutton.Click += new System.EventHandler(this.PREVIOUSbutton_Click);
+            // 
+            // NEXTbutton
+            // 
+            this.NEXTbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NEXTbutton.Location = new System.Drawing.Point(447, 389);
+            this.NEXTbutton.Name = "NEXTbutton";
+            this.NEXTbutton.Size = new System.Drawing.Size(139, 36);
+            this.NEXTbutton.TabIndex = 10;
+            this.NEXTbutton.Text = "next";
+            this.NEXTbutton.UseVisualStyleBackColor = true;
+            this.NEXTbutton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(249, 266);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 32);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Gender";
+            // 
+            // GENDERtextbox
+            // 
+            this.GENDERtextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GENDERtextbox.Location = new System.Drawing.Point(486, 266);
+            this.GENDERtextbox.Name = "GENDERtextbox";
+            this.GENDERtextbox.Size = new System.Drawing.Size(169, 30);
+            this.GENDERtextbox.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.GENDERtextbox);
+            this.Controls.Add(this.NEXTbutton);
+            this.Controls.Add(this.PREVIOUSbutton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.STANDARDtextbox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.AGEtextbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NAMEtextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IDtextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox IDtextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox NAMEtextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox AGEtextbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox STANDARDtextbox;
+        private System.Windows.Forms.Button PREVIOUSbutton;
+        private System.Windows.Forms.Button NEXTbutton;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox GENDERtextbox;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        studentDBDataContext db;
+        List<student> lst;
+        int index;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           /* studentDBDataContext db= new studentDBDataContext();
+            DataGrid.datasource = db.students;
+            Table<student> stdtable = db.students;
+            IDataGrid.source = stdtable;*/
+            db=new studentDBDataContext();
+            lst= db.students.ToList();
+            Display();
+
+
+
+        }
+        public void Display()
+        {
+            IDtextBox.Text = lst[index].Id.ToString();
+            NAMEtextbox.Text = lst[index].name;
+            AGEtextbox.Text = lst[index].age.ToString();
+            GENDERtextbox.Text = lst[index].gender;
+            STANDARDtextbox.Text = lst[index].standard.ToString();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (index < lst.Count-1)
+            {
+                index+=1;
+                Display();
+            }
+            else
+                MessageBox.Show("this is last element");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PREVIOUSbutton_Click(object sender, EventArgs e)
+        {
+            if (index > 0)
+            {
+                index -= 1;
+                Display();
+            }
+            else
+                MessageBox.Show("this is first element");
+        }
+    }
+}
